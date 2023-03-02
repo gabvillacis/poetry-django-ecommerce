11fdeb40193797a939715c9975e8cb3503e1031c
Gabriel Villacis <55613545+gabvillacis@users.noreply.github.com>
2023-03-02 17:30:50 -0500

    Update forms.py

--- store/forms.py
+++ store/forms.py
@@ -10,7 +10,7 @@ class SignUpForm(forms.Form):
     address = forms.CharField(label="Dirección:", max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Ej.: Av. Quito 392 y Vélez'}))
     cellphone = forms.CharField(label="Celular:", min_length=10, max_length=13, required=True, widget=forms.TextInput(attrs={'placeholder': 'Ej.: 0995851023'}))
     password1 = forms.CharField(label="Contraseña:", min_length=8, max_length=16, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Ej.: de*738Hoewiux!$'}))
-    password2 = forms.CharField(label="Confirme la contraseña:", min_length=8, max_length=16, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Ej.: de*738Hoewiux!$'}))
+    password2 = forms.CharField(label="Repita la contraseña:", min_length=8, max_length=16, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Ej.: de*738Hoewiux!$'}))
     
     def clean(self):
         cleaned_data = super(SignUpForm, self).clean()
